38488da8f3dea1e79c54eb60ea5285fc4992dc1b
kurt shuey <kurts1@umbc.edu>
2021-08-23 14:52:57 -0400

    Add loading of json from input element

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
-var labels = jsondata.map((e)=> e.index);
-var linedata = jsondata.map((e)=> e.count);
+function runApp(){
+    $("#option1").click(()=>{ 
+      UpdateChart(jsondata1);
+    });
+    $("#option2").click(()=>{ 
+      UpdateChart(jsondata2);
+    });
+    $("#fileLoader").on("change", ()=>{
+      var reader = new FileReader();
+      reader.onload = ()=> {
+        UpdateChart(JSON.parse(reader.result));
+      };
+      reader.readAsText($("#fileLoader").prop('files')[0]);
+    });
+}
 
-var chart = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(chart, {
+
+function UpdateChart(jsonString){
+  //delete&remake to reuse canvas
+   var chartDiv = $('#myChartDiv');
+  chartDiv.html("<canvas id=\"myChart\" width=\"600\" height=\"400\"></canvas>");
+  
+  //split Json for chart js 
+  var xAxis = jsonString.map((e)=> e.index);
+  var yAxis = jsonString.map((e)=> e.count);
+ 
+  var chart = $('#myChart')[0].getContext('2d');
+  var myChart = new Chart(chart, {
     type: 'line',
     data:  { 
-        labels: labels, 
-        datasets:[{
-            label: "indexXcount", 
-            data : linedata,
-            borderColor: '#c1b0d9',
-            backgroundColor : '#6432a8'
-        }]
+      labels: xAxis, 
+      datasets:[{
+          label: "indexXcount", 
+          data : yAxis,
+          borderColor: '#c1b0d9',
+          backgroundColor : '#6432a8'
+      }]
     },
     options: 
     { 
-         scales: {
+      scales: {
         x: {
           display: true,
           title: {
             display: true,
-            text: 'index'
+            text: 'Index'
           }},
           y: {
             display: true,
             title: {
               display: true,
-              text: 'count'
+              text: 'Count'
             }}
         },
-        maintainAspectRatio: false
+      maintainAspectRatio: false,
+      responsive: false
     }
-});
+  });
+}
